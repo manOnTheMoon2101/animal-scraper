649aaa02d556abee174f17d013c7e40ceae9065d
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-05-10 21:15:16 +0200

    done for now

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ModeToggle } from "./Header/Theme_Toggle/toggle";
+import Image from "next/image";
+import dog from "@/public/dog.jpg";
 
 function Header() {
   return (
@@ -7,6 +9,15 @@ function Header() {
       <div className="flex justify-end mx-4">
         <ModeToggle />
       </div>
+      <div className="flex justify-center h-48">
+        <Image
+          alt="Photo by Gilberto Reyes: https://www.pexels.com/photo/closeup-photo-of-short-coated-white-and-gray-dog-825947/"
+          src={dog}
+          priority={true}
+          style={{ objectFit: "fill" }}
+        />
+      </div>
+      real ui design
     </div>
   );
 }
