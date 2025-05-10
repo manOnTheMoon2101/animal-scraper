cc86a893d047d0386ac0dff6e628b56869b7c6f2
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-05-10 20:24:40 +0200

    f a database

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['wellingtonspca.co.za'],
+  },
 };
 
-export default nextConfig;
+export default nextConfig; 
