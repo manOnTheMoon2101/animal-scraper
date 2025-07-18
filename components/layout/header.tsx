278644c9c5dcbb29512d810a0c29f3bafa28b058
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-07-18 20:35:52 +0200

    nice

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,11 +6,16 @@ import dog from "@/public/dog.jpg";
 import RandomTip from "./Header/Tips/tips";
 import { Dog, Cat } from "lucide-react";
 import { McLaren } from "next/font/google";
+import { Fira_Sans } from "next/font/google";
 import { Badge } from "../ui/badge";
 import Link from "next/link";
 import { toast } from "sonner";
 const mclaren = McLaren({
   weight: "400",
+  subsets: ["latin"],
+});
+const fira = Fira_Sans({
+  weight: "200",
   subsets: ["latin"],
 });
 function Header() {
@@ -63,12 +68,12 @@ function Header() {
           </div>
         </div>
       </div>
-      <div className="flex flex-row justify-center text-center text-2xl md:text-6xl text-gray-600 mx-2 my-48">
-        <p className={` ${mclaren.className}`}>
+      <div className="flex flex-row justify-center text-left text-2xl md:text-6xl text-gray-600 mx-2 my-48">
+        <p className={` ${fira.className}`}>
           You can make a difference by adopting one of our lovely animals and
           helping us on our way to give every animal a
           <span className="flex flex-col md:inline-flex md:flex-row items-center">
-            <span className="underline text-secondary"> friendly</span>,
+            <span className="underline text-secondary font-bold"> friendly</span>,
             welcoming and safe home
             <Dog
               size={48}
@@ -79,7 +84,7 @@ function Header() {
       </div>
 
       <div className="my-24 mx-2">
-        <span className="text-xl text-gray-600 text-left italic">
+        <span className={`"text-xl text-gray-600 text-left  ${fira.className}`}>
           Bona fide monetary donations qualify for an 18A tax certificate and
           entitles you to a tax deduction in respect of your donation. Donors
           who are South African taxpayers are entitled to deduct the total value
@@ -87,12 +92,13 @@ function Header() {
           SPCA, in any tax year, up to a limit of 10% of their taxable income
           for that tax year. Should you require an 18A tax certificate in lieu
           of for your kind donation, please email us at{" "}
-          <Badge className="cursor-pointer" onClick={copyToClipboard}>
+          <Badge className="cursor-pointer italic" onClick={copyToClipboard}>
             info@wellingtonspca.co.za
           </Badge>{" "}
           together with the proof of donation. or visit{" "}
           <Badge className="cursor-pointer">
             <Link
+              className="italic"
               href="https://wellingtonspca.co.za/help/"
               target="_blank"
               rel="noopener noreferrer"
